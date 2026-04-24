93c6e9d56e85388c8d82cef9832e53660b89b610
WooDoo1337 <slaviiklll@mail.ru>
2026-04-24 20:23:43 +1000

    ... - 744

--- educa/educa/settings/base.py
+++ educa/educa/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-tu^s0sh5nv&&pvm%r)8^vxr%_!hkj4au^o@6ae@g077b87g^)v'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
@@ -82,15 +81,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'educa.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
---
+++ educa/educa/settings/prod.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+
+ADMINS = [
+    ('slavik', 'slaviiklll@mail.ru'),
+]
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+    }
+}
