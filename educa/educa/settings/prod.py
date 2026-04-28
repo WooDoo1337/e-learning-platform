47f2d1adce487b494e1d54139fd907391ad6f031
WooDoo1337 <slaviiklll@mail.ru>
2026-04-28 21:02:45 +1000

    ... - 756

--- educa/educa/settings/prod.py
+++ educa/educa/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import *
+import os
 
 DEBUG = False
 
@@ -10,5 +11,15 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
